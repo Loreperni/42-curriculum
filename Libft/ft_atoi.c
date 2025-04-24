1562182ecc440c97ed38691c1efd24d385c87267
Loreperni <Lorenzo.perniciano@bywaves.it>
2025-04-24 16:23:35 +0200

    added atoi

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,23 @@
+int	ft_atoi(const *str)
+{
+	int	i;
+	int	is_neg;
+	int	res;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] != '\t' || str[i] != '\n' || str[i] != '\r'
+		|| str[i] != '\f' || str[i] != ' ')
+		i++;
+	if (str[i] == '-')
+		is_neg = -1;
+	else
+		is_neg = 1;
+	if (is_neg == -1 || str[i] == '+')
+		i++;
+	res = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = (res * 10) + (str[i++] - '0');
+	return (res * is_neg);
+}
