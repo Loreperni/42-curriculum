ee59ce8b27dfc1ce16bb16f97f46640a4e0f7a62
Lorenzo Perniciano <lpernici@c3r8s1.42malaga.com>
2025-04-24 12:54:29 +0200

    added: isalnum, isalpha, isascii, isdigit, isprint, strlcat, strcpy, strlen, strncmp, tolower, toupper

---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpernici <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 11:42:40 by lpernici          #+#    #+#             */
+/*   Updated: 2025/04/24 12:03:30 by lpernici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcat(char *dest, char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	i;
+
+	dest_len = 0;
+	src_len = 0;
+	i = 0;
+	while (dest[dest_len] != 0 && dest_len < size - 1)
+		dest_len++;
+	while (src[src_len] != 0)
+		src_len++;
+	if (dest_len >= size)
+		return (size + src_len);
+	while (src[i] != '\0' && dest_len + i < size - 1)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
